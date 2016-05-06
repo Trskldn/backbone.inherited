ad25c6cab26a402fcefd16d6b4e34c2a4d3d7d80
Taras Kolodny <trskldn@gmail.com>
2016-05-06 17:56:28 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # BackboneInheritedMixin
 
-'''javascript
-		M1 = Backbone.Model.extend(_.extend({
+```javascript
+		Model1 = Backbone.Model.extend(_.extend({
 			someMethod: function(){
 				done();
 				return 'M1.someMethod returned value';
 			}
 		}, InheritedMixin));
 
-		M2 = M1.extend();
-		M3 = M2.extend({
+		Model2 = Model1.extend();
+		Model3 = Model2.extend({
 			someMethod:function(){ 
 				return this.inherited('someMethod', arguments);
 			}
 		});
 
-		m3 = new M3();
-		m3.someMethod();
-'''
+		model3 = new Model3();
+		model3.someMethod(); //invoks Model3.someMethod() - > Model1.someMethod()
+```
--- test/test-backbone.inherited.js
+++ test/test-backbone.inherited.js
@@ -6,21 +6,21 @@ var Backbone = require('backbone');
 describe('InheritedMixin', function() {
 	it('this.inherited("someMethod") should call parents prototype method', function(done) {
 
-		M1 = Backbone.Model.extend(_.extend({
+		Model1 = Backbone.Model.extend(_.extend({
 			someMethod: function(){
 				done();
 				return 'M1.someMethod returned value';
 			}
 		}, InheritedMixin));
 
-		M2 = M1.extend();
-		M3 = M2.extend({
+		Model2 = Model1.extend();
+		Model3 = Model2.extend({
 			someMethod:function(){ 
 				return this.inherited('someMethod', arguments);
 			}
 		});
 
-		m3 = new M3();
-		m3.someMethod();
+		model3 = new Model3();
+		model3.someMethod();
 	});
 });
